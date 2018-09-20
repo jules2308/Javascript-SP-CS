6e9b5c0e531ec9f1fc78ce02805f4761a227bab2
Juge2308 <jules.geurs@hotmail.com>
2018-09-20 13:23:05 -0600

    Update

--- swapping variables/index.js
+++ swapping variables/index.js
@@ -1,18 +1,12 @@
-console.log('Hello World');
 
-// Create 10 variables about yourself
 let firstname = "Jules";
 let lastname = "Geurs";
-let age = 17;
-const ageafraid = 29; //security feature
-let hair = null; // undefined, null, origonally "none"
-// DEBug with typeof
-let male = true;
+
+// new code goes here
+
+firstname = "Geurs";
+lastname ="Jules";
 
 // Print these out to console
 console.log(firstname);
 console.log(lastname);
-console.log(age);
-console.log(ageafraid);
-console.log(hair);
-console.log("Am I male? " + male);
