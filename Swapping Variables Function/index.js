fdeecacb67b24b44688773595de6b651526e4607
Juge2308 <jules.geurs@hotmail.com>
2018-09-24 13:29:47 -0600

    Update

--- Swapping Variables Function/index.js
+++ Swapping Variables Function/index.js
@@ -1,4 +1,6 @@
-// Create 10 variables about yourself
+// Your function goes here
+
+
 let JulesGeurs = [];
 let MeganSpence = [];
 
@@ -6,13 +8,12 @@ let MeganSpence = [];
 JulesGeurs = ["Jules", "Geurs", 13, 29, null, true, false];
 MeganSpence = ["Megan", "Spence", 14, null, true, false, true,];
 
-// Print out entire object
-console.log(JulesGeurs);
-console.log(MeganSpence);
+// Your code goes here
+
 
 // Print first names only
-console.log("last Name is: " + JulesGeurs[1]);
-console.log("Last Name is: " + MeganSpence[1]);
+console.log("last Name of JulesGeurs is: " + JulesGeurs[1]);
+console.log("Last Name of MeganSpence is: " + MeganSpence[1]);
 
 JulesGeurs[1] = "Is Awesome";
 console.log(JulesGeurs)[1];
