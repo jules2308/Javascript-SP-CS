e8d04bc862e47f0f1a8b80dff3dbf486307e5ec8
Juge2308 <jules.geurs@hotmail.com>
2018-10-04 12:34:56 -0600

    Update

---
+++ Modulus Assignments/index.js
@@ -0,0 +1,12 @@
+function simpleOperators (10, 2){
+  let adding = number[A] + number[B];
+  let subtract = number[A] - number[B];
+  let mutliply = number[A] * number[B];
+  let divide = number[A] / number[B];
+  console.log(adding);
+  console.log(subtract);
+  console.log(mutliply);
+  console.log(divide);
+}
+
+let number = [10, 2];
