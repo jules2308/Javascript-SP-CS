5c14493fe0de06bd0b1298e4872062164aaec9cb
Jules2308 <jules.geurs@hotmail.com>
2018-10-08 19:57:37 -0600

    Update

--- Modulus Assignments/index.js
+++ Modulus Assignments/index.js
@@ -1,28 +1,53 @@
-function simpleOperators (numberA, numberB){
-  let adding = numberA + numberB;
-  let subtract = numberA - numberB;
-  let mutliply = numberA * numberB;
-  let divide = numberA / numberB;
-  console.log(adding);
-  console.log(subtract);
-  console.log(mutliply);
-  console.log(divide);
+let number = [90, 30, 89, 2];
+let skipcount = number[3]
+
+
+function skipCounting (skipCount, times, startingNumber){
+  let number = startingNumber - skipCount;
+ for (let i=0; i<=times; i++){
+    number -= skipCount;
+    console.log(number);
+  }
+}
+function odd (even, odd){
+let startingNumber = number[0];
+for (let i=0; i<=difference; i++){
+even = (startingNumber += skipcount) % 2;
+  console.log("This number is ", even, "; This iteration is number", i+skipCount);
+}
 }
 
-let number = [7, 8];
-let example = [15, 19];
-simpleOperators (number[0], number[1]);
-simpleOperators (example[0], example[1]);
+function simpleOperators (number[0], number[1]){
+  let subtract = number[0] - number[1];
+  console.log(subtract);
+}
+
+
+//////////
+
+skipCounting (2, 20, 90)
 
 
 
 
 
-//let number = [___, ___]; //Boundary Numbers
-//let skipCount = ___; //Number of skipCounting between each element of the sequence, used in FOR Loop
-//let difference = number[] - number[]; //Difference, mandatory expectation
-//let startingNumber = number[]; //Hint: used in the FOR Loop
-//for (let i=0; i<=difference; i++){
-  //even = (startingNumber += skipCount) % 2;
-  //console.log("This number is", even, "; This iteration is number", i+skipCount); //Example of Concatenating
+//let number = [7, 8];
+//let example = [15, 19];
+//simpleOperators (number[0], number[1]);
+//simpleOperators (example[0], example[1]);
+
+
+/////////////////
+
+//function simpleOperators (numberA, numberB){
+//  let subtract = numberA - numberB;
+//  console.log(subtract);
+//}
+
+//function skipCounting (skipCount, times, startingNumber){
+//  let number = startingNumber - skipCount;
+//  for (let i=1; i<=times; i++){
+//    number -= skipCount;
+//    console.log(number);
+//  }
 //}
