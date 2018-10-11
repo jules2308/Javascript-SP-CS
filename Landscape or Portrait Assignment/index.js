f242da7590dc4ebc25fc74b6ebb8ad719ef64d52
Juge2308 <jules.geurs@hotmail.com>
2018-10-11 12:23:32 -0600

    Update

--- Landscape or Portrait Assignment/index.js
+++
@@ -1,8 +0,0 @@
-function isLandscape (width, height) {
-  if (width == height ) return 'Square'; //must be first, return will not allow rest of code to run
-  return (width > height) ? 'Landscape' : 'Portrait'; //returns the value of the expression
-}
-
-let width = 800;
-let height = 800;
-console.log("This display geometry is", isLandscape (width, height)); //Difference between calling functions with arguements and sending to parameter's, local variables
