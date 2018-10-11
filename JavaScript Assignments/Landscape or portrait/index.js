4ff81a5aaec9245698f114298533d50ced599098
Juge2308 <jules.geurs@hotmail.com>
2018-10-11 12:25:06 -0600

    Update

--- JavaScript Assignments/Landscape or portrait/index.js
+++
@@ -1,8 +0,0 @@
-function isLandscape (width, height) {
-  if (width == height) return 'Square';
-  return (width > height) ? 'Landscape' : 'Portrait';
-}
-
-let width = 1200;
-let height = 400;
-console.log("This display geometry is", isLandscape (width, height));
