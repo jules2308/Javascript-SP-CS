0e3405b4ef93343b604c4051d0db7a3e9a18e076
Juge2308 <jules.geurs@hotmail.com>
2018-10-12 15:32:38 -0600

    Update

--- JavaScript Assignments/Demerit Points/index.js
+++ JavaScript Assignments/Demerit Points/index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-checkSpeed (); //add value here
-
-function checkSpeed (speed) {
-  speedLimit = ; //add value here, remember to initialize with constant not let for security purposes
-  kmPerPoint = ; //add value here
-  if (speed <= ) return console.log ('Ok'); //Add formula for calculating the acceptable speed from above variables
-  //after return, none of the other code will run, there is no need to report demerit points
-
-  // This variable and formula are const (constant) for security so it cannot be changed, for example when cross-scripting
-  const points = Math.floor (); //Add formula for calculating the point demerits
-  if () return console.log ('License Suspended'); //Add conditional to check for points to suspend license
-=======
 checkSpeed (71); //add value here
 
 function checkSpeed (speed) {
@@ -20,8 +7,7 @@ function checkSpeed (speed) {
   //after return, none of the other code will run, there is no need to report demerit points
 
   // This variable and formula are const (constant) for security so it cannot be changed, for example when cross-scripting
-  const points = Math.floor (kmperpoint += ????????? ); //Add formula for calculating the point demerits
+  const points = Math.floor (speedlimit += kmperpoint ); //Add formula for calculating the point demerits
   if (points >= 12) return console.log ('License Suspended'); //Add conditional to check for points to suspend license
->>>>>>> 88e05478cc9693fe2645b83124e44a6a936a1b60
   else console.log ('Points', points)
 }
--- JavaScript Assignments/Ordering Grades/index.js
+++ JavaScript Assignments/Ordering Grades/index.js
@@ -1,9 +1,5 @@
 //Variable is an array for security
-<<<<<<< HEAD
-const marks = []; //add grades as an array
-=======
 const marks = [30, 60]; //add grades as an array
->>>>>>> 88e05478cc9693fe2645b83124e44a6a936a1b60
 
 //Use the calculatedGrade function as an output to the console
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
@@ -11,15 +7,6 @@ console.log ("My letter grade is", calculatedGrade (average), "because my numeri
 
 //This function uses another function, call Single Concern Function (able to use either function for various purposes)
 function calculatedGrade (marks) {
-  const average = calculateAverage (marks);
-<<<<<<< HEAD
-  if (average < 60) return ''; //Add appropriate grade or feedback
-  if () return ''; //Add approriate conditional
-  if () return '';
-  if () return '';
-  return A; //This is the last grade left, using the funnel image (metaphor)
-}
-=======
   if (average >= 90) return 'A'; //Add appropriate grade or feedback
   if (average < 90) return 'B'; //Add approriate conditional
   if (average < 80) return 'C';
@@ -27,7 +14,6 @@ function calculatedGrade (marks) {
   return F; //This is the last grade left, using the funnel image (metaphor)
 }
 
->>>>>>> 88e05478cc9693fe2645b83124e44a6a936a1b60
 function calculteAverage (array) {
   let sum = 0;
   for (let mark of marks) sum += mark; //Example of reading an array and one line to add up the sum of all the grades
