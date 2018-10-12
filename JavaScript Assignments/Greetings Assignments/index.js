88e05478cc9693fe2645b83124e44a6a936a1b60
Jules2308 <jules.geurs@hotmail.com>
2018-10-11 20:52:52 -0600

    Update

--- JavaScript Assignments/Greetings Assignments/index.js
+++ JavaScript Assignments/Greetings Assignments/index.js
@@ -1,6 +1,6 @@
-let hour = 1; //Variable is in hours only
+let hour = 12; //Variable is in hours only
 
-if (hour >= 6 && hour < 12) console.log('Good Morning');
+if (hour >= 4 && hour < 12) console.log('Good Morning');
 else if (hour >= 12 && hour < 18) console.log('Good Afternoon');
-else if (hour >= 18 && hour < 24 || 0) console.log('Good Evening');
-else (hour >= 0 && hour < 6) console.log('Good Evening');
+else if (hour >= 18 && hour <= 24) console.log('Good Evening');
+else if (hour >= 0 && hour < 4) console.log('Good Evening');
